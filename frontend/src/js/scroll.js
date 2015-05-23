cc5165f2e3c899963df3422e7cb2c600003d0524
MaritzaVst <maritzasintitafur@gmail.com>
2015-05-22 20:44:02 -0500

    Carrusel de la sección the goods al 70%

--- frontend/src/js/scroll.js
+++ frontend/src/js/scroll.js
@@ -15,20 +15,49 @@ $(document).ready(function(){
 
 /*Carrousel*/
 
-// jQuery
-$gallery.flickity( 'next', isWrapped )
-// vanilla JS
-flkty.next( isWrapped )
+$(document).ready( function() {
+  // vanilla JS
+  var $gallery = $('.main-gallery .gallery').flickity();
+  $('.main-gallery').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true
+    });
 
-$(document).ready(function() {
-  var $gallery = $('.gallery').flickity();
+   var $gallery2 = $('.second-gallery .gallery').flickity();
+  $('.second-gallery').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true
+    });
 
-  // previous
-  $('.button--previous').on('click', function() {
-    $gallery.flickity('previous');
+  $('.button--previous').on( 'click', function() {
+
+    $('.main-gallery').flickity('previous');
   });
   // previous wrapped
-  $('.button--previous-wrapped').on('click', function() {
-    $gallery.flickity('previous', true);
+  $('.button--previous-wrapped').on( 'click', function() {
+    $('.main-gallery').flickity( 'previous', true );
   });
+
+// GO TO PREVIOUS
+  // $('.main-gallery button.previous').on( 'click', function() {
+
+  //   $('.second-gallery').flickity('previous');
+  // });
+  // previous wrapped
+ $('.main-gallery button.previous').on( 'click', function() {
+   $('.second-gallery').flickity( 'previous', true );
+ });
+
+// GO TO NEXT
+  //  $('.main-gallery button.next').on( 'click', function() {
+
+  //   $('.second-gallery').flickity('next');
+  // });
+  // previous wrapped
+ $('.main-gallery button.next').on( 'click', function() {
+   $('.second-gallery').flickity( 'next', true );
+ });
+  
 });
