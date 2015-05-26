3bbd7d3c9603f6c7d0a7f9ce7aa7c7639f405ef0
MaritzaVst <maritzasintitafur@gmail.com>
2015-05-26 15:45:47 -0500

    Menú identifica secciones

--- frontend/src/js/scroll.js
+++ frontend/src/js/scroll.js
@@ -24,9 +24,9 @@ $(document).ready(function(){
 /*
   Collapse-Faq's
 */
-$('#change-arrow').click(function() {
-    $("#up").toggleClass("hidden");
-    $("#down").toggleClass('hidden');
+$('.change-arrow').click(function() {
+    $(" #up").toggleClass("hidden");
+    $(" #down").toggleClass('hidden');
 });
 
 /*Carrousel*/
@@ -64,3 +64,47 @@ $('.gallery-nav').flickity({
   contain: true,
   pageDots: false
 });
+
+
+
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+   $('.contenedor-menu a[href^="#"]').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href").toString());
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.contenedor-menu nav a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
