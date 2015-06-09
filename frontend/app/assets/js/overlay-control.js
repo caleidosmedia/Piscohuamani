ed4877316dd33a84ce9737b3dc59e5f606d205f3
MaritzaVst <maritzasintitafur@gmail.com>
2015-06-09 12:26:00 -0500

    Correcciones terminadas

--- frontend/app/assets/js/overlay-control.js
+++ frontend/app/assets/js/overlay-control.js
@@ -2,10 +2,11 @@
   POPUP EVENTS
 */
 var $overlay = $('#overlay');
-var $image = $('#overlay img');
+var $image = $('#overlay .img-overlay');
 var $next = $('#overlay .next-overlay');
 var $previous = $('#overlay .previous-overlay');
-
+var $images = $('.grid-image').length;
+var $close = $('.close');
 
 $(document).ready(function(){
 	$('.grid ul li a').click(function (event) {
@@ -13,22 +14,38 @@ $(document).ready(function(){
 	  var href = $(this).attr('href');
 	  $image.attr("src", href);
 	  $overlay.addClass("flex");
+	  $index = $(this).attr('data-image-index');
+	  setGalleryURLS($index);
 	  $overlay.show();
 	});
-	$overlay.click(function(){ 
-	   $overlay.hide();
-	   $overlay.removeClass("flex");
+
+	$close.click(function(){
+		$overlay.hide();
+		$overlay.removeClass("flex");
 	});
-	
+
 	$next.click(function (event){
 		event.preventDefault();
-		var href = $(this).attr('href');
-	  $image.attr("src", href[1]);
-	  $overlay.show();
+		$index = $(this).attr('data-image-index');
+		$image.attr("src", $(".grid-image-"+($index)).attr("href"))
+	  setGalleryURLS($index);
 	});
+
 	$previous.click(function(event){
 		event.preventDefault();
-		var href = $(this).attr('href');
-	  var captionText = $(this).children("img").attr("alt");
+		$index = $(this).attr('data-image-index');
+		$image.attr("src", $(".grid-image-"+($index)).attr("href"))
+	  setGalleryURLS($index);
 	});
 });
+
+function setGalleryURLS($index){
+
+	 if ($index>0 &&  $index<($images+1)) {
+	  $previous.attr("data-image-index", parseInt($index)-1);
+		$previous.attr("href", $(".grid-image-"+($index-1)).attr("href"));
+	
+	  $next.attr("href", $(".grid-image-"+($index+1)).attr("href"));
+	  $next.attr("data-image-index", parseInt($index)+1);
+	}
+}
