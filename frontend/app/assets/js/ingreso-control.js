b63d767e97ca5cb4b94acde00e6889434f920800
MaritzaVst <maritzasintitafur@gmail.com>
2015-08-07 16:40:21 -0500

    Menu nav del blog clickeable

--- frontend/app/assets/js/ingreso-control.js
+++ frontend/app/assets/js/ingreso-control.js
@@ -1,3 +1,11 @@
+/*INGRESO*/
 $(".btn-language").click(function() {
   $(this).toggleClass("active-btn");
+});
+
+/*BLOG MENU2*/
+$(".btn-select").click(function(event) {
+	event.preventDefault()
+  $(".btn-select").removeClass("active-select");
+  $(this).toggleClass("active-select");
 });
