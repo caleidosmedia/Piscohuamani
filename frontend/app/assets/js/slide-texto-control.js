fdb5a36d8c4b30ecb38d0981ff58d6f15ea8c000
MaritzaVst <maritzasintitafur@gmail.com>
2015-08-11 15:32:46 -0500

    Texto con opacity de events-index

--- frontend/app/assets/js/slide-texto-control.js
+++ frontend/app/assets/js/slide-texto-control.js
@@ -1,43 +1,70 @@
-/*
-  POPUP EVENTS
-*/
-var $slider = $('#slider-text-prueba');
-// var $images = $('.grid-image').length;
-var $next = $('#events container-sm gallery-button-right');
-var $previous = $('#events container-sm gallery-button-left');
+var contentLength = $(".text-opacity").length; 
 
-$(document).ready(function(){
-	$(next).click(function (event) {
-	  event.preventDefault();
-	  var slideContenido = document.getElementById('text-opacity').innerHTML;
-	  $(slider).append(slideContenido);
-	  $index = $(this).attr('data-index');
-	  setGalleryURLS($index);
-	  $slider.show();
-	});
-
-	$next.click(function (event){
-		event.preventDefault();
-		$index = $(this).attr('data-index');
-		$slider.attr("src", $(".grid-image-"+($index)).attr("href"))
-	  setGalleryURLS($index);
-	});
-
-	$previous.click(function(event){
-		event.preventDefault();
-		$index = $(this).attr('data-image-index');
-		$image.attr("src", $(".grid-image-"+($index)).attr("href"))
-	  setGalleryURLS($index);
-	});
+$(document).ready(function() {
+	mostrarContenido();
 });
 
-function setGalleryURLS($index){
 
-	 if ($index>0 &&  $index<($images+1)) {
-	  $previous.attr("data-image-index", parseInt($index)-1);
-		$previous.attr("href", $(".grid-image-"+($index-1)).attr("href"));
-	
-	  $next.attr("href", $(".grid-image-"+($index+1)).attr("href"));
-	  $next.attr("data-image-index", parseInt($index)+1);
-	}
+$("#slider_back").click(function(e) {
+	$("#slider-text-oficial").addClass("opacity-active");
+	setTimeout(function() {
+		interpolar("back")
+	}, 300);
+});
+
+$("#slider_next").click(function(e) {
+	$("#slider-text-oficial").addClass("opacity-active");
+	setTimeout(function() {
+		interpolar("next")
+	}, 300);
+});
+
+function mostrarContenido() {
+	var current_element = obtenerContenidoActual();
+	if (current_element != null) {
+		$("#slider-text-oficial").html(current_element.html());
+		setTimeout(function() {
+			$("#slider-text-oficial").removeClass("opacity-active");
+		}, 200);
+	};
+}
+
+function obtenerContenidoActual() {
+	var current_element = null;
+	$(".text-opacity").each(function(index, element) {
+		var current = $(element).attr("data-current");
+		if (current == 1) {
+			current_element = $(element);
+			return;
+		};
+	});
+	return current_element;
+}
+
+function obtenerContenidoPorIndice(indice) {
+	var content = null;
+	$(".text-opacity").each(function(index, element) {
+		var index = $(element).attr("data-index");
+		if (index == indice) {
+			content = $(element);
+			return;
+		};
+	});
+	return content;
+}
+
+function interpolar(accion) {
+	var current_element = obtenerContenidoActual();
+	var index = $(current_element).attr("data-index");
+	if (accion == "next") {
+		if (index == contentLength) { index = 0;};
+		index++;	
+	} else {
+		if (index == 1) { index = contentLength + 1;};
+		index--;
+	};
+	var element = obtenerContenidoPorIndice(index);
+	$(current_element).attr("data-current", "0");
+	$(element).attr("data-current", "1");
+	mostrarContenido();
 }
