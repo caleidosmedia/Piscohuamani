d927ea2335b0570901aefe2ef5d0f0fdc522b7d6
MaritzaVst <maritzasintitafur@gmail.com>
2015-08-17 10:16:24 -0500

    Validación de edad legal segun c/ país lista

--- frontend/app/assets/js/forms-control.js
+++ frontend/app/assets/js/forms-control.js
@@ -2,7 +2,6 @@ var numericReg = /^[0-9\b]+$/;
 var current_year = new Date().getFullYear();
 var current_day = new Date().getDate();
 var current_month = new Date().getMonth();
-var year = $("#year");
 
 var paises = "";
 function loadJson(callback) {
@@ -31,56 +30,73 @@ $(document).ready(function() {
   /*Validación edad*/
   hola();
 
-  $(year).change(function(){
-    if (year.val().length === 4) {
-      var min_age = 0;
-      $("#country").change(function(){
-        var selected_country = $("#country option:selected").text();
-        console.log(selected_country);
-        for (var i = 0; i < paises.length; i++) {
-          if (paises[i].country === selected_country) {
-            min_age = paises[i].age;
-            console.log(min_age);
-            return;
-          };
-        var newYear = $(year).val();
-         if ((current_year - $(newYear).parseInt() > min_age)) {
-            $(".enter").removeAttr("disabled");
-            $(".enter").addClass("active-enter");
-          }
-          else {
-              $(".enter").Attr("disabled");
-              $(".enter").removeClass("active-enter");
-            }  
-         
-        };
+  $("#country").change(function(){
+    validarEdad();
+  });
 
-        // if ((current_year - $(year).val() <= min_age)) {
-        //   var year_1 = $(year).val();
-        //   var month_1 = parseInt($(month).val());
-        //   var day_1 = parseInt($(day).val());
-        //   var days_to_birthday = 0;
-        //   var days_to_today = 0;
-        //   for (var i = 1; i < month_1; i++) {
-        //     days_to_birthday += new Date(year_1, month_1-i, 0).getDate();
-        //   };
-        //   days_to_birthday += day_1;
-        //   console.log(days_to_birthday);
-        //   for (var i = 0; i < current_month; i++) {
-        //     days_to_today += new Date(current_year, current_month-i, 0).getDate();
-        //   };
-        //   days_to_today += current_day;
-        //   console.log(days_to_today); 
-        //   if (days_to_today < days_to_birthday) {
-        //     $(".enter").attr("disabled", "disabled");
-        //     $(".enter").removeClass("active-enter");
-        //   };    
-        // }
-        
-      });
-    }
+  $("#year").keyup(function(){
+    validarEdad();
+  });
+
+  $("#month").keyup(function(){
+    validarEdad();
+  });
+
+  $("#day").keyup(function(){
+    validarEdad();
   });
 }); 
+
+function validarEdad() {
+  var year = $("#year").val();
+  var month = $("#month").val();
+  var day = $("#day").val();
+  var country = $("#country").val();
+  if (year.length == 4 && month.length >= 1 && day.length >= 1 && country.length != 0) {
+    var min_age = 0;
+    var selected_country = $("#country option:selected").text();
+    console.log(selected_country);
+    for (var i = 0; i < paises.length; i++) {
+      if (paises[i].country.toString().toUpperCase() === selected_country.toString().toUpperCase()) {
+        min_age = paises[i].age;
+        console.log(min_age);
+        break;
+      };     
+    };
+    if ((current_year - parseInt(year) == min_age)) {
+      var year_1 = parseInt(year);
+      var month_1 = parseInt(month);
+      var day_1 = parseInt(day);
+      var days_to_birthday = 0;
+      var days_to_today = 0;
+      for (var i = 1; i < month_1; i++) {
+        days_to_birthday += new Date(year_1, month_1-i, 0).getDate();
+      };
+      days_to_birthday += day_1;
+      console.log(days_to_birthday);
+      for (var i = 0; i < current_month; i++) {
+        days_to_today += new Date(current_year, current_month-i, 0).getDate();
+      };
+      days_to_today += current_day;
+      console.log(days_to_today); 
+      if (days_to_today < days_to_birthday) {
+        $(".enter").attr("disabled", "disabled");
+        $(".enter").removeClass("active-enter");
+      } else {
+        $(".enter").removeAttr("disabled");
+        $(".enter").addClass("active-enter");
+      }
+    } else if ((current_year - parseInt(year) > min_age)) {
+      $(".enter").removeAttr("disabled");
+      $(".enter").addClass("active-enter");
+      console.log("Si");
+    }
+    else {
+      $(".enter").attr("disabled");
+      $(".enter").removeClass("active-enter");
+    }  
+  }
+}
 
 /*Número máximo y autojump*/
 function hola() {
