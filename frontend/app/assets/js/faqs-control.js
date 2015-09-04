9f6005c4b288447758148b07d81d750e85b4fa0b
MaritzaVst <maritzasintitafur@gmail.com>
2015-09-04 09:43:23 -0500

    Hero-image de index-html modificado

--- frontend/app/assets/js/faqs-control.js
+++ frontend/app/assets/js/faqs-control.js
@@ -8,9 +8,9 @@ $('.change-arrow').click(function(){
 
 
 
-var backgrounds = ["assets/img/fotos/foto-home.png", "https://images.unsplash.com/reserve/Af0sF2OS5S5gatqrKzVP_Silhoutte.jpg?q=80&fm=jpg&s=aa0399d0a07be6afd5470a6dd6092bb3","https://images.unsplash.com/photo-1438109491414-7198515b166b?q=80&fm=jpg&s=cbdabf7a79c087a0b060670a6d79726c", "https://unsplash.com/photos/t-W4_309hi8/download", "https://unsplash.imgix.net/photo-1436915947297-3a94186c8133?fit=crop&fm=jpg&q=75&w=1050", "https://unsplash.imgix.net/photo-1435459025078-9857f8933bc9?fit=crop&fm=jpg&h=1050&q=75&w=1050", "https://ununsplash.imgix.net/photo-1434828927397-62ea053f7a35?fit=crop&fm=jpg&h=700&q=75&w=1050"]
+var backgrounds = ["assets/img/fotos/foto-home.png", "assets/img/fotos/IMG_0212.png","assets/img/fotos/IMG_0230.png", "assets/img/fotos/IMG_0279.png"]
 
 $(document).ready(function () {
-	var i = Math.floor(Math.random()*5)
-	$("#background").attr("style", "background-image: url("+backgrounds[i]+")")
+	var i = Math.floor(Math.random()*4)
+	$("#background").attr("style", "background-image: url("+backgrounds[i]+"); background-position-y:30%");
 });
