e4b7fa5b92a710d89ede98f7d63365db1beffb47
MaritzaVst <maritzasintitafur@gmail.com>
2015-09-10 12:21:59 -0500

    Fotos de events y el hero-image cambiadasy optimizada y formulario de contact modificado

--- frontend/app/assets/js/faqs-control.js
+++ frontend/app/assets/js/faqs-control.js
@@ -8,7 +8,7 @@ $('.change-arrow').click(function(){
 
 
 
-var backgrounds = ["assets/img/fotos/foto-home.png", "assets/img/fotos/IMG_0212.png","assets/img/fotos/IMG_0230.png", "assets/img/fotos/IMG_0279.png"]
+var backgrounds = ["assets/img/fotos/foto-home.png", "assets/img/fotos/IMG_0212.png","assets/img/fotos/hero2.jpg", "assets/img/fotos/hero1.jpg"]
 
 $(document).ready(function () {
 	var i = Math.floor(Math.random()*4)
