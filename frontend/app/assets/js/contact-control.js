0e5c956f1c9e078d84c5eaa799a4d70e67a4eda1
MaritzaVst <maritzasintitafur@gmail.com>
2015-09-11 15:28:13 -0500

    Validación de contacto con html5 y mensajes de error

--- frontend/app/assets/js/contact-control.js
+++ frontend/app/assets/js/contact-control.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-	$(".send a").click(function(e){
-		e.preventDefault();
-		$(".overlay-contact").addClass("active-pop");
-	});
+
+	var email = document.querySelector('input[type="email"]');
+	var name = document.querySelector('input[type="text"]');
+	var message = document.querySelector('textarea[name="textarea"]');
+
+	validar(email, "Please enter a valid e-mail address", "This field is required");
+	validar(name, "Please enter a valid name", "This field is required");
+	validar(message, "m", "Please write a message");
+
+	function validar(inputName, addInformation, invalid) {
+		inputName.oninvalid = function(e) {
+			e.target.setCustomValidity("");
+			if (!e.target.validity.valid) {
+				if (e.target.value.length != 0) {
+					e.target.setCustomValidity(addInformation);
+				} else {
+						e.target.setCustomValidity(invalid);
+				}
+			}
+		};
+	}
 });
