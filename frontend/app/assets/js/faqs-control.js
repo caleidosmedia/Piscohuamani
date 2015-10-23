00782fd76527ef0b58979ed7fca2a14fefc0eaec
MaritzaVst <maritzasintitafur@gmail.com>
2015-10-23 10:23:52 -0500

    commit de prueba

--- frontend/app/assets/js/faqs-control.js
+++ frontend/app/assets/js/faqs-control.js
@@ -1,12 +1,16 @@
 /*
   COLLAPSE FAQS
 */
-$('.change-arrow').click(function(){
+$('.change-arrow').click(function(event){
 	$(this).parent().find('#up').toggleClass("hidden");
-	$(this).parent().find('#down').toggleClass("hidden");	
+	$(this).parent().find('#down').toggleClass("hidden");
+	var target = $( event.target );
+	// $(target).parent(".panel-default").find(".panel-collapse").toggleClass("in");
 });
 
-
+$(document).ready(function(){
+	
+});
 
 var backgrounds = ["assets/img/fotos/foto-home.png", "assets/img/fotos/IMG_0212.png","assets/img/fotos/hero2.jpg", "assets/img/fotos/hero1.jpg"]
 
