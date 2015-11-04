4844496787555681c706c20105ed2bd20d0278db
MaritzaVst <maritzasintitafur@gmail.com>
2015-11-04 11:21:03 -0500

    randon de imagenes de header en template

--- frontend/app/assets/js/faqs-control.js
+++ frontend/app/assets/js/faqs-control.js
@@ -12,9 +12,9 @@ $(document).ready(function(){
 	
 });
 
-var backgrounds = ["assets/img/fotos/foto-home.png", "assets/img/fotos/IMG_0212.png","assets/img/fotos/hero2.jpg", "assets/img/fotos/hero1.jpg"]
+// var backgrounds = ["assets/img/fotos/foto-home.png", "assets/img/fotos/IMG_0212.png","assets/img/fotos/hero2.jpg", "assets/img/fotos/hero1.jpg"]
 
-$(document).ready(function () {
-	var i = Math.floor(Math.random()*4)
-	$("#background").attr("style", "background-image: url("+backgrounds[i]+"); background-position-y:30%");
-});
+// $(document).ready(function () {
+// 	var i = Math.floor(Math.random()*4)
+// 	$("#background").attr("style", "background-image: url("+backgrounds[i]+"); background-position-y:30%");
+// });
