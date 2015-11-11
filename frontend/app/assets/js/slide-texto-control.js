17aafd91c050b97e31896eb0f92e1a9558977bbf
MaritzaVst <maritzasintitafur@gmail.com>
2015-11-11 10:55:17 -0500

    Event: ronda-1 -> sin data current

--- frontend/app/assets/js/slide-texto-control.js
+++ frontend/app/assets/js/slide-texto-control.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$(".ronda-1:first-child, .text-opacity1:first-child, .text-opacity2:first-child").attr("data-current","1");
+	$("text-opacity1:first-child, .text-opacity2:first-child").attr("data-current","1");
 	/*EVENTS*/
 	if($('.events-slide[data-current="1"]').size() == 0){
 		$(".text-opacity:first-child").attr("data-current","1");
