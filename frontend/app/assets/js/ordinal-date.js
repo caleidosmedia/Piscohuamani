54ef5ab3b68b0f3055d52d28fae3c1f07b92149b
MaritzaVst <maritzasintitafur@gmail.com>
2015-11-25 15:49:20 -0500

    EVENT Y BLOG: variable de fecha cambiada

--- frontend/app/assets/js/ordinal-date.js
+++ frontend/app/assets/js/ordinal-date.js
@@ -15,6 +15,24 @@ function ordinalDate(date) {
     }
 }
 var dateNumber = $(".fecha-evento").html().substr(4);
-if (dateNumber < 32) {
-    $(".fecha-evento").append("<sup>"+ ordinalDate(dateNumber) +"</sup>");
-}
+$(".fecha-evento").each(function(){
+    if (dateNumber < 32) {
+      $(".fecha-evento").append("<sup>"+ ordinalDate(dateNumber) +"</sup>");
+    }
+});
+
+// var dateNumber = $(".fecha-evento").html().substr(4);
+// $(".fecha-evento").each(function(){
+//   if (dateNumber= 1 || dateNumber= 21 || dateNumber= 31) {
+//     $(".fecha-evento").append("<sup>st</sup>");
+//   }
+//   else if (dateNumber= 2 || dateNumber= 22) {
+//     $(".fecha-evento").append("<sup>nd</sup>");
+//   }
+//   else if (dateNumber= 3 || dateNumber= 23) {
+//     $(".fecha-evento").append("<sup>rd</sup>");
+//   }
+//   else {
+//     $(".fecha-evento").append("<sup>th</sup>");
+//   }
+// });
