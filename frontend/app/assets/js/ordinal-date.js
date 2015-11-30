f3a28a1aabf033c1fbc87a166f2483c99e39788c
MaritzaVst <maritzasintitafur@gmail.com>
2015-11-30 12:09:10 -0500

    EVENTS: Fecha con sufijo ordinal

--- frontend/app/assets/js/ordinal-date.js
+++ frontend/app/assets/js/ordinal-date.js
@@ -1,38 +1,52 @@
-function ordinalDate(date) {
-    switch (date) {
-    case 1:
-    case 21:
-    case 31:
-        return 'st';
-    case 2:
-    case 22:
-        return 'nd';
-    case 3:
-    case 23:
-        return 'rd';
-    default:
-        return 'th';
-    }
-}
-var dateNumber = $(".fecha-evento").text().split(" ").pop();
+// function ordinalDate(date) {
+//     switch (date) {
+//     case 1:
+//     case 21:
+//     case 31:
+//         return 'st';
+//     break;  
+//     case 2:
+//     case 22:
+//         return 'nd';
+//     break; 
+//     case 3:
+//     case 23:
+//         return 'rd';
+//     break; 
+//     default:
+//         return 'th';
+//     break; 
+//     }
+// }
+
 // $(".fecha-evento").each(function(){
-    if (dateNumber < 32) {
-      $(".fecha-evento").append("<sup>"+ ordinalDate(dateNumber) +"</sup>");
-    }
+//   var dateNumber = $(".fecha-evento").text().split(" ").pop();
+//     if (dateNumber == 01 || dateNumber == 21 || dateNumber == 31) {
+//       $(this).append("<sup>st</sup>");
+//     }
+//     else if (dateNumber == 02 || dateNumber == 22) {
+//       $(this).append("<sup>nd</sup>");
+//     }
+//     else if (dateNumber == 03 || dateNumber == 23) {
+//       $(this).append("<sup>rd</sup>");
+//     }
+//     else if{
+//       $(".fecha-evento").append("<sup>th</sup>");
+//     }
 // });
 
-// var dateNumber = $(".fecha-evento").html().substr(4);
-// $(".fecha-evento").each(function(){
-//   if (dateNumber= 1 || dateNumber= 21 || dateNumber= 31) {
-//     $(".fecha-evento").append("<sup>st</sup>");
-//   }
-//   else if (dateNumber= 2 || dateNumber= 22) {
-//     $(".fecha-evento").append("<sup>nd</sup>");
-//   }
-//   else if (dateNumber= 3 || dateNumber= 23) {
-//     $(".fecha-evento").append("<sup>rd</sup>");
-//   }
-//   else {
-//     $(".fecha-evento").append("<sup>th</sup>");
-//   }
-// });
+// // $(".fecha-evento").each(function(){
+// //   var dateNumber = $(".fecha-evento").text().split(" ").pop();
+// //   if (dateNumber == 01 || dateNumber == 21 || dateNumber == 31) {
+// //     $(this).append("<sup>st</sup>");
+// //   }
+// //   else if (dateNumber == 02 || dateNumber == 22) {
+// //     $(this).append("<sup>nd</sup>");
+// //   }
+// //   else if (dateNumber == 03 || dateNumber == 23) {
+// //     $(this).append("<sup>rd</sup>");
+// //   }
+// //   else {
+// //     $(this).append("<sup>th</sup>");
+// //   }
+// // });
