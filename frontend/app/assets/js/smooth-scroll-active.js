b385379d44010216e96ea488f24d6654d2248e8b
Renzo Telenta <rtelenta@caleidos.pe>
2016-12-15 16:49:42 -0500

    cambios

--- frontend/app/assets/js/smooth-scroll-active.js
+++ frontend/app/assets/js/smooth-scroll-active.js
@@ -2,17 +2,39 @@
   SMOOTH SCROLL ACTIVE
 */
 $(document).ready(function () {
+  var flag = true;
+  
+  function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    if (flag) {
+      $('.contenedor-menu a.active-hover[href^="#"]').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href").toString());
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('.contenedor-menu nav a').removeClass("active");
+          currLink.addClass("active");
+          //console.log("dsada");
+        }
+        else{
+          currLink.removeClass("active");
+        }
+      });
+    }
+  }
+
   $(document).on("scroll", onScroll);
+
   $("nav.smooth a, a.smooth").click(function(e){
-    $(document).off("scroll");
+    flag = false;
     $(this).addClass("active");
     $("html, body").stop().animate({
       scrollTop: $($(this).attr("href")).offset().top - 80
     }, 1000, function () {
-      $(document).on("scroll", onScroll);
+      flag = true;
     });
     e.preventDefault();
   });
+
   /*$('.active-hover').on('click', function (e) {
     e.preventDefault();
     $(document).off("scroll");
@@ -34,18 +56,3 @@ $(document).ready(function () {
     });
   });*/
 });
-function onScroll(event){
-  var scrollPos = $(document).scrollTop();
-  $('.contenedor-menu a.active-hover[href^="#"]').each(function () {
-    var currLink = $(this);
-    var refElement = $(currLink.attr("href").toString());
-    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-      $('.contenedor-menu nav a').removeClass("active");
-      currLink.addClass("active");
-      //console.log("dsada");
-    }
-    else{
-      currLink.removeClass("active");
-    }
-  });
-}
