8cb03867b2ccb888a1b8d30add719ab975bc2158
Renzo Telenta <rtelenta@caleidos.pe>
2016-12-15 16:51:13 -0500

    cambios

--- frontend/app/assets/js/smooth-scroll-active.js
+++ frontend/app/assets/js/smooth-scroll-active.js
@@ -2,47 +2,14 @@
   SMOOTH SCROLL ACTIVE
 */
 $(document).ready(function () {
-  var flag = true;
-  
-  function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    if (flag) {
-      $('.contenedor-menu a.active-hover[href^="#"]').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href").toString());
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.contenedor-menu nav a').removeClass("active");
-          currLink.addClass("active");
-          //console.log("dsada");
-        }
-        else{
-          currLink.removeClass("active");
-        }
-      });
-    }
-  }
-
   $(document).on("scroll", onScroll);
-
-  $("nav.smooth a, a.smooth").click(function(e){
-    flag = false;
-    $(this).addClass("active");
-    $("html, body").stop().animate({
-      scrollTop: $($(this).attr("href")).offset().top - 80
-    }, 1000, function () {
-      flag = true;
-    });
-    e.preventDefault();
-  });
-
-  /*$('.active-hover').on('click', function (e) {
+  $('.active-hover').on('click', function (e) {
     e.preventDefault();
     $(document).off("scroll");
     
     $('a').each(function () {
         $(this).removeClass('active');
     })
-
     $(this).addClass('active');
   
     var target = this.hash,
@@ -54,5 +21,19 @@ $(document).ready(function () {
         window.location.hash = target;
         $(document).on("scroll", onScroll);
     });
-  });*/
+  });
 });
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.contenedor-menu a.active-hover[href^="#"]').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href").toString());
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('.contenedor-menu nav a').removeClass("active");
+      currLink.addClass("active");
+    }
+    else{
+      currLink.removeClass("active");
+    }
+  });
+}
