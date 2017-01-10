23d149198e19b0004071d86f9274a9939192d2a7
Renzo Telenta <rtelenta@caleidos.pe>
2017-01-10 15:18:30 -0500

    cambios

--- frontend/app/assets/js/overlay-control.js
+++ frontend/app/assets/js/overlay-control.js
@@ -41,16 +41,30 @@ var ready = function(){
 function setGalleryURLS($index){
 	var $images = $('#container-ronda .grid-image').length;
 	 if ($index>0 &&  $index<($images+1)) {
-	  $previous.attr("data-image-index", parseInt($index)-1);
-	  $previous.attr("href", $("#container-ronda .grid-image-"+($index-1)).attr("href"));
-	
-	  $next.attr("href", $("#container-ronda .grid-image-"+($index+1)).attr("href"));
-	  $next.attr("data-image-index", parseInt($index)+1);
-	} else if ($index === 1) {
-		$previous.hide();
-	} else if ($index === $images) {
-		$show.hide();
-	}
+  	  if (parseInt($index) === 1) {
+  	  	$index = $images-1;
+  	  } else if (parseInt($index) === $images) {
+
+  	  }
+
+  	  if (parseInt($index) === 1) {
+  	  	$previous.attr("data-image-index", $images-1);
+	  	$previous.attr("href", $("#container-ronda .grid-image-"+$images-1).attr("href"));
+  	  } else {
+  	  	$previous.attr("data-image-index", parseInt($index)-1);
+	  	$previous.attr("href", $("#container-ronda .grid-image-"+($index-1)).attr("href"));
+  	  }
+	  
+	  
+	  if (parseInt($index) === $images) {
+	    $next.attr("href", $("#container-ronda .grid-image-"+1).attr("href"));
+	    $next.attr("data-image-index", 1);
+  	  } else {
+  	  	$next.attr("href", $("#container-ronda .grid-image-"+($index+1)).attr("href"));
+	    $next.attr("data-image-index", parseInt($index)+1);
+  	  }
+	  
+	} 
 }
 
 
